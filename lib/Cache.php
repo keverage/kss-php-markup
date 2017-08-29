138f6e761a0ff613ef7aa4234eb30ed6c99cf4e4
keverage <dev@kevinbaubet.fr>
2017-08-29 19:39:56 +0200

    Add libs

---
+++ lib/Cache.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Kss\Markup;
+
+/**
+ * Class Cache
+ *
+ * @package Kss\Markup
+ */
+class Cache
+{
+    /**
+     * @var string
+     */
+    protected $path = __DIR__ . '/../cache';
+
+    /**
+     * Cache constructor.
+     */
+    public function __construct()
+    {
+        $this->prepare();
+    }
+
+    /**
+     * Prepare cache folder
+     */
+    public function prepare()
+    {
+        if (!file_exists($this->path)) {
+            umask(0000);
+            mkdir($this->path, 0755, true);
+        }
+    }
+
+    /**
+     * Get file path with cache key
+     *
+     * @param $key
+     * @return string
+     */
+    public function getFilePath($key)
+    {
+        return $this->path . '/' . $key;
+    }
+
+    /**
+     * Set cache data
+     *
+     * @param $key
+     * @param $data
+     */
+    public function set($key, $data) {
+        $file = $this->getFilePath($key);
+
+        if ($handle = fopen($file, 'w')) {
+            fwrite($handle, $data);
+            fclose($handle);
+        }
+    }
+
+    /**
+     * Get cache data
+     *
+     * @param $key
+     * @return bool|string
+     */
+    public function get($key)
+    {
+        $file = $this->getFilePath($key);
+
+        if (file_exists($file)) {
+            $data = file_get_contents($file);
+
+            if (!empty($data)) {
+                return $data;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Remove one cache file
+     *
+     * @param $key
+     * @return bool
+     */
+    public function remove($key)
+    {
+        return unlink($this->getFilePath($key));
+    }
+
+    /**
+     * Clean all cache
+     */
+    public function clean()
+    {
+        $directory = scandir($this->path);
+
+        if (!empty($directory)) {
+            foreach ($directory as $item) {
+                if ($item !== '.' && $item !== '..') {
+                    $this->remove($item);
+                }
+            }
+        }
+    }
+}
---
+++ lib/Markup.php
@@ -0,0 +1,258 @@
+<?php
+
+namespace Kss;
+
+/**
+ * Class Markup
+ *
+ * @package Kss
+ */
+class Markup
+{
+    /**
+     * @var \StdClass
+     */
+    protected $options;
+
+    /**
+     * @var null
+     */
+    protected $url = null;
+
+    /**
+     * @var null
+     */
+    protected $selector = null;
+
+    /**
+     * @var
+     */
+    protected $filter;
+
+    /**
+     * @var null
+     */
+    protected $markup = null;
+
+    /**
+     * @var \Kss\Markup\Cache
+     */
+    protected $cache;
+
+    /**
+     * Markup constructor.
+     *
+     * @param \Kss\Section $section
+     * @param \StdClass    $options
+     */
+    public function __construct(\Kss\Section $section, \StdClass $options = null)
+    {
+        if (!class_exists('PHPHtmlParser\Dom')) {
+            return false;
+        }
+
+        // Cache
+        $this->cache = new Markup\Cache;
+
+        // Set
+        $this->set($section->getMarkup());
+
+        // Options
+        $this->options = new \StdClass;
+        $this->options->cache = false;
+        $this->options->modifier = '$modifierClass';
+        $this->options->urlPrefix = null;
+
+        if (!empty($options)) {
+            $this->setOptions($options);
+        }
+
+        // Auto markup
+        if ($this->isUrl()) {
+            // Filter
+            $this->filter = new \StdClass;
+            $this->filter->length = 2;
+
+            // Parse and set
+            $this->parse()->setFromUrl();
+        }
+
+        // Cache?
+        if (!$this->options->cache) {
+            $this->flushcache();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param \stdClass $options
+     *
+     * @return $this
+     */
+    public function setOptions(\stdClass $options)
+    {
+        foreach ($options as $key => $value) {
+            if (property_exists($this->options, $key)) {
+                $this->options->{$key} = $value;
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return \StdClass
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * @param $option
+     *
+     * @return bool
+     */
+    public function getOption($option)
+    {
+        if (!empty($this->options->{$option})) {
+            return $this->options->{$option};
+        }
+
+        return false;
+    }
+
+    /**
+     * @return $this
+     */
+    public function parse()
+    {
+        $matches = [];
+        preg_match_all('@url\((.*)\)->find\(([-\.#\w]+)\)(?:->filter\(([-\.#\w]+)(?:,(.+))?\))?@', $this->get(), $matches);
+
+        $this->url = $this->getOption('urlPrefix');
+        if (!empty($matches[1][0])) {
+            $this->url .= trim($matches[1][0], ' \'"');
+        }
+        if (!empty($matches[2][0])) {
+            $this->selector = trim($matches[2][0], ' \'"');
+        }
+        if (!empty($matches[3][0])) {
+            $this->filter->selector = trim($matches[3][0], ' \'"');
+        }
+        if (!empty($matches[4][0])) {
+            $this->filter->length = (int) trim($matches[4][0]);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function setFromUrl()
+    {
+        // Cache?
+        $cacheRebuild = true;
+        if ($this->options->cache) {
+            $cacheKey = md5($this->get());
+            $cacheData = $this->cache->get($cacheKey);
+
+            if (!empty($cacheData)) {
+                $cacheRebuild = false;
+                $this->set($cacheData);
+            }
+        }
+
+        // Get dom
+        if ($cacheRebuild) {
+            $dom = new \PHPHtmlParser\Dom;
+            $dom->load($this->url, array(
+                'preserveLineBreaks' => true
+            ));
+            $element = $dom->find($this->selector)[0];
+
+            if (!empty($element)) {
+                // Filter?
+                if (!empty($this->filter->selector)) {
+                    $newElement = new \PHPHtmlParser\Dom;
+                    $newElement->load($element->outerHtml, array(
+                        'preserveLineBreaks' => true
+                    ));
+
+                    $filter = $newElement->find($this->filter->selector);
+                    $filterParent = null;
+
+                    if (!empty($filter)) {
+                        $i = 0;
+                        foreach ($filter as $item) {
+                            if ($i >= $this->filter->length) {
+                                $item->delete();
+                            }
+                            $i++;
+                        }
+                    }
+                    unset($filter);
+
+                    $html = $newElement->outerHtml;
+
+                } else {
+                    $html = $element->outerHtml;
+                }
+
+                $this->set($html);
+
+                if ($this->options->cache && !empty($cacheKey)) {
+                    $this->cache->set($cacheKey, $html);
+                }
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isUrl()
+    {
+        return (substr($this->get(), 0, 4) === 'url(');
+    }
+
+    /**
+     * @param $markup
+     */
+    public function set($markup)
+    {
+        $this->markup = $markup;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function get()
+    {
+        return $this->markup;
+    }
+
+    /**
+     * @param string $replacement
+     *
+     * @return mixed
+     */
+    public function getNormal($replacement = '')
+    {
+        return str_replace($this->getOption('placeholder'), $replacement, $this->get());
+    }
+
+    /**
+     * Flush markup cache
+     */
+    public function flushcache()
+    {
+        return $this->cache->clean();
+    }
+}
